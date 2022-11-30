d8964a530cdc1f21b45ad749b787513d8038b5bd
yancz2000 <yancz2000@163.com>
2022-11-30 22:57:31 +0800

    Update redis-check-aof.c

--- src/redis-check-aof.c
+++ src/redis-check-aof.c
@@ -508,14 +508,6 @@ void checkOldStyleAof(char *filepath, int fix, int preamble) {
 static sds checkAofVersion(void) {
     sds version;
     version = sdscatprintf(sdsempty(), "%s", REDIS_VERSION);
-
-    /* Add git commit and working tree status when available */
-    /*if (strtoll(redisGitSHA1(),NULL,16)) {
-        version = sdscatprintf(version, " (git:%s", redisGitSHA1());
-        if (strtoll(redisGitDirty(),NULL,10))
-            version = sdscatprintf(version, "-dirty");
-        version = sdscat(version, ")");
-    }*/
     return version;
 }
 

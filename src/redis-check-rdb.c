1e902f0b912ab7dc955fd1390f791dfec458582b
yancz2000 <yancz2000@163.com>
2022-12-01 01:10:10 +0800

    Update redis-check-rdb.c

--- src/redis-check-rdb.c
+++ src/redis-check-rdb.c
@@ -455,6 +455,6 @@ int redis_check_rdb_main(int argc, char **argv, FILE *fp) {
 
 usage:
     //fprintf(stderr, "Usage: %s <rdb-file-name>\n", argv[0]);
-    printf("Usage: %s <rdb-file-name>\n", argv[0]);
+    //printf("Usage: %s <rdb-file-name>\n", argv[0]);
     exit(1);
 }
